51d5da70edb38ac419d045fc420bcb634b241a47
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-08-10 19:50:17 +0500

    adding backend url to the frontend

--- Frontend/src/Components/lib/axiosInstance.js
+++ Frontend/src/Components/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from"axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://ai-micro-mentor-fod5.vercel.app//api",
     withCredentials:true,
 })
 
