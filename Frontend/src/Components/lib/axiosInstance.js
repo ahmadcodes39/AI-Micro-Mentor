05dfc85907ba52f3ea4a9bea3d4a100a362ba2c6
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-08-10 19:54:38 +0500

    correcting url

--- Frontend/src/Components/lib/axiosInstance.js
+++ Frontend/src/Components/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from"axios"
 
 const api = axios.create({
-    baseURL:"https://ai-micro-mentor-fod5.vercel.app//api",
+    baseURL:"https://ai-micro-mentor-fod5.vercel.app/api",
     withCredentials:true,
 })
 
